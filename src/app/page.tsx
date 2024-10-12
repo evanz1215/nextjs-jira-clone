5332e7eebd05cdb61d77795346b962fe96245326
Evan <evanz1215@gmail.com>
2024-10-12 14:27:20 +0800

    調整預設樣式

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,19 @@
 import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
 
 export default function Home() {
   return (
     <div className="flex gap-4">
-      <Button>Primary</Button>
+      <Input />
+      <Button variant="primary" size="xs">
+        Primary
+      </Button>
       <Button variant="secondary">Secondary</Button>
       <Button variant="destructive">Destructive</Button>
       <Button variant="ghost">Ghost</Button>
+      <Button variant="muted">Muted</Button>
+      <Button variant="outline">Outline</Button>
+      <Button variant="tertiary">Tertiary</Button>
     </div>
   );
 }
