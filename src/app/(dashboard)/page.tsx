dab306096ef3c4d791db5623597c307bec32e1fc
Evan <evanz1215@gmail.com>
2024-10-17 16:07:43 +0800

    Refactor component file structure and add sign-in and sign-up pages

--- src/app/(dashboard)/page.tsx
+++ src/app/(dashboard)/page.tsx
@@ -1,5 +1,4 @@
 import { getCurrent } from "@/features/auth/actions";
-import UserButton from "@/features/auth/components/user-button";
 import { redirect } from "next/navigation";
 
 export default async function Home() {
@@ -9,9 +8,5 @@ export default async function Home() {
     redirect("/sign-in");
   }
 
-  return (
-    <div>
-      <UserButton></UserButton>
-    </div>
-  );
+  return <div>This is a home page</div>;
 }
